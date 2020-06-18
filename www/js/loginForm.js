4ff1e03ca5f74d849ba05180409b5ecb2d7ceac1
James Bithell <james@bithell.com>
2020-06-18 20:21:48 +0100

    Log request issues

--- www/js/loginForm.js
+++ www/js/loginForm.js
@@ -32,6 +32,7 @@ ons.ready(function() {
                         }
                     },
                     error: function(request, status, error) {
+                        console.log(resquest);
                         ons.notification.alert(request.statusText);
                     }
                 });
