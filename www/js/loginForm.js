b49210caddc9fdeb1c1a1c263e25a184eac427a4
James Bithell <james@bithell.com>
2020-07-01 16:27:43 +0100

    Move away from an ONS ready event for the login

--- www/js/loginForm.js
+++ www/js/loginForm.js
@@ -1,46 +1,44 @@
-ons.ready(function() {
-    $("#login-submitbtn").click(function () {
-        var username = $("#login-username").val();
-        var password = $("#login-password").val();
-        var networkState = navigator.connection.type;
-        if (networkState === Connection.NONE) {
-            ons.notification.toast("No Network Connection", { timeout: 2000 });
-        } else {
-            if (username != '' && password != '') {
-                $.ajax({
-                    type: "POST",
-                    url: myApp.config.endpoint + 'auth/appLogin.php',
-                    dataType: 'json',
-                    data: {
-                        'email': username,
-                        'password': password,
-                        'deviceData':
-                            'Device Model:' + device.model + ',' +
-                            'Device Cordova:'  + device.cordova  + ',' +
-                            'Device Platform:' + device.platform + ',' +
-                            'Device UUID:'     + device.uuid     + ',' +
-                            'Device Version:'  + device.version  + ',' +
-                            'Connection Type:' + networkState
-                    },
-                    success: function(response) {
-                        if (response.result) {
-                            localStorage.setItem('token',response.response.token);
-                            myApp.auth.token = response.response.token;
-                            myApp.functions.launchApp();
-                        } else {
-                            ons.notification.toast(response.error.message, { timeout: 3000 });
-                        }
-                    },
-                    error: function(request, status, error) {
-                        console.log(JSON.stringify(request));
-                        console.log(JSON.stringify(error));
-                        console.log(JSON.stringify(status));
-                        ons.notification.alert(request.statusText);
+myApp.auth.login = function () {
+    var username = $("#login-username").val();
+    var password = $("#login-password").val();
+    var networkState = navigator.connection.type;
+    if (networkState === Connection.NONE) {
+        ons.notification.toast("No Network Connection", {timeout: 2000});
+    } else {
+        if (username != '' && password != '') {
+            $.ajax({
+                type: "POST",
+                url: myApp.config.endpoint + 'auth/appLogin.php',
+                dataType: 'json',
+                data: {
+                    'email': username,
+                    'password': password,
+                    'deviceData':
+                        'Device Model:' + device.model + ',' +
+                        'Device Cordova:' + device.cordova + ',' +
+                        'Device Platform:' + device.platform + ',' +
+                        'Device UUID:' + device.uuid + ',' +
+                        'Device Version:' + device.version + ',' +
+                        'Connection Type:' + networkState
+                },
+                success: function (response) {
+                    if (response.result) {
+                        localStorage.setItem('token', response.response.token);
+                        myApp.auth.token = response.response.token;
+                        myApp.functions.launchApp();
+                    } else {
+                        ons.notification.toast(response.error.message, {timeout: 3000});
                     }
-                });
-            } else {
-                ons.notification.toast("Please enter a username and password", { timeout: 2000 });
-            }
+                },
+                error: function (request, status, error) {
+                    console.log(JSON.stringify(request));
+                    console.log(JSON.stringify(error));
+                    console.log(JSON.stringify(status));
+                    ons.notification.alert(request.statusText);
+                }
+            });
+        } else {
+            ons.notification.toast("Please enter a username and password", {timeout: 2000});
         }
-    });
-});
+    }
+}
