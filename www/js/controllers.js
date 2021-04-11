3ca33658ae1f80b84f4b6e66422f125e63cb2a25
James Bithell <james@bithell.com>
2021-04-11 16:12:23 +0100

    Add pull-to-refresh to asset type pages

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -54,7 +54,7 @@ myApp.controllers = {
                                     });
                                 });
                             }
-                            if (myApp.auth.instanceHasPermission(17)) {
+                            if (myApp.auth.instanceHasPermission(17) && false) {
                                 $("#menu-asset-addNewButton").show();
                             }
                             if (myApp.auth.instanceHasPermission(85)) {
@@ -154,26 +154,22 @@ myApp.controllers = {
                         ons.notification.toast("Sorry barcode not found", { timeout: 2000 });
                     }
                 } else {
-                    if (typeof myApp.data.assetTypes[assetResult.asset.assetTypes_id] === "undefined") {
-                        //We don't currently have this one downloaded so lets go grab it
-                        var requestData = {"assetTypes_id": assetResult.asset.assetTypes_id,"all":true };
-                        myApp.functions.apiCall("assets/list.php", requestData, function (assetDownloadResult) {
-                            $(assetDownloadResult['assets']).each(function (index, element) {
-                                if (typeof myApp.data.assetTypes[element['assetTypes_id']] === "undefined") { //Shouldn't realy be needed
-                                    myApp.data.assetTypes[element['assetTypes_id']] = element;
-                                    myApp.controllers.assets.fullAssetListAppend(element);
-                                }
-                            });
-                            if (Object.keys(assetDownloadResult['assets']).length > 0) {
-                                myApp.controllers.assets.barcodeScanAddToList(assetResult.asset.assetTypes_id, assetResult.asset.assets_id);
-                            } else {
-                                //Asset wasn't found
-                                ons.notification.toast("Sorry asset not found - is the correct business set?", { timeout: 2000 });
+                    //Go and get the data on this asset - yeah we may have it already but we should go get it again in case something has changed
+                    var requestData = {"assetTypes_id": assetResult.asset.assetTypes_id,"all":true };
+                    myApp.functions.apiCall("assets/list.php", requestData, function (assetDownloadResult) {
+                        $(assetDownloadResult['assets']).each(function (index, element) {
+                            if (typeof myApp.data.assetTypes[element['assetTypes_id']] === "undefined") { //Shouldn't realy be needed
+                                myApp.controllers.assets.fullAssetListAppend(element);
                             }
+                            myApp.data.assetTypes[element['assetTypes_id']] = element;
                         });
-                    } else {
-                        myApp.controllers.assets.barcodeScanAddToList(assetResult.asset.assetTypes_id, assetResult.asset.assets_id);
-                    }
+                        if (Object.keys(assetDownloadResult['assets']).length > 0) {
+                            myApp.controllers.assets.barcodeScanAddToList(assetResult.asset.assetTypes_id, assetResult.asset.assets_id);
+                        } else {
+                            //Asset wasn't found
+                            ons.notification.toast("Sorry asset not found - is the correct business set?", { timeout: 2000 });
+                        }
+                    });
                 }
             });
         },
@@ -185,7 +181,7 @@ myApp.controllers = {
                     return false;
                 }
             });
-            $("#scanned-list").prepend('<ons-list-item tappable modifier="longdivider"  onclick="document.querySelector(\'#myNavigator\').pushPage(\'assetType.html\', {data: {id: ' + typeid + '}}).then(function() { document.querySelector(\'#myNavigator\').pushPage(\'asset.html\', {data: {id: ' + typeid + ', asset: ' + id + '}}) });">' +
+            /*$("#scanned-list").prepend('<ons-list-item tappable modifier="longdivider"  onclick="document.querySelector(\'#myNavigator\').pushPage(\'assetType.html\', {data: {id: ' + typeid + '}}).then(function() { document.querySelector(\'#myNavigator\').pushPage(\'asset.html\', {data: {id: ' + typeid + ', asset: ' + id + '}}) });">' +
                 '<div class="left">' +
                 (myApp.data.assetTypes[typeid].thumbnails.length > 0 ? '<img loading="lazy" class="list-item__thumbnail" src="' + myApp.data.assetTypes[typeid].thumbnails[0]['url'] + '">' : '<span style="width: 40px;"></span>')+
                 '</div>' +
@@ -193,7 +189,7 @@ myApp.controllers = {
                 '<div class="right">' +
                 '<div class="list-item__label">' + (typeof myApp.auth.location.name !== "undefined" ? myApp.auth.location.name : "") + '</div>'+
                 '</div>' +
-                '</ons-list-item>');
+                '</ons-list-item>');*/
             document.querySelector('#myNavigator').pushPage('assetType.html', {data: {id: typeid}}).then(function () {
                 document.querySelector('#myNavigator').pushPage('asset.html', {data: {id: typeid, asset: id }});
             });
@@ -206,11 +202,11 @@ myApp.controllers = {
             if (clear) {
                 //Clear all assets
                 $("#allAssetsList").html("");
-                $("#scanned-list").html("");
+                //$("#scanned-list").html("");
                 myApp.data.assetTypes = {};
                 myApp.data.assetTypesPages = null;
             }
-            var requestData = {"page": Math.floor(Object.keys(myApp.data.assetTypes).length / 20)+1};
+            var requestData = {"page": Math.floor(Object.keys(myApp.data.assetTypes).length / 20)+1,"abridgedList":true,"imageComp":"tiny"};
             if (myApp.data.assetTypesPages != null && Object.keys(myApp.data.assetTypes).length > ((myApp.data.assetTypesPages-1)*20)) {
                 //Don't allow it to duplicate objects
                 $("#allAssetsListLoader").hide();
@@ -240,7 +236,7 @@ myApp.controllers = {
                 '</div>' +
                 '<div class="center"><span class="list-item__title">' + element['assetTypes_name'] + '</span><span class="list-item__subtitle">' + element['assetCategories_name'] + ' - ' + element['manufacturers_name'] + '</span></div>' +
                 '<div class="right">' +
-                '<div class="list-item__label">' + (element['tags'].length > 1 ? 'x' + element['tags'].length : element['tags'][0]['assets_tag_format'].replace("-","&#8209;"))+'</div>'+
+                '<div class="list-item__label">' + (element['count'] > 1 ? 'x' + element['count'] : element['tags'][0]['assets_tag_format'].replace("-","&#8209;"))+'</div>'+
                 '</div>' +
                 '</ons-list-item>');
             return true;
@@ -260,8 +256,10 @@ myApp.controllers = {
                 $("#projectPageDescription").html("Client: " + myApp.data.projects[data.data.id]['clients_name']);
             } else $("#projectPageDescription").html("");
         },
-        newAssetPage: function (data) {
-            myApp.functions.log(data);
+        editAssetPage: function (data) {
+            if (data.data.id == "NEW") {
+                $("#editAssetPageTitle").html("New Asset");
+            }
         },
         about: function (data) {
             $('.versionNumber').text(myApp.config.version.number);
@@ -269,58 +267,81 @@ myApp.controllers = {
             $('#aboutPagePlatform').text(device.platform + " (" + device.model + ")");
         },
         assetTypePage: function (data) {
-            var thisAsset = myApp.data.assetTypes[data.data.id];
-            if (thisAsset) {
-                $("#assetTypePageTitle").html(thisAsset['assetTypes_name']);
-                $("#assetTypePageManufacturer").html(thisAsset['manufacturers_name']);
-                $("#assetTypePageCategory").html(thisAsset['assetCategories_name']);
-                $("#assetTypePageDescription").html(myApp.functions.nl2br(thisAsset['assetTypes_description']));
-                $("#assetTypePageProductLink").html(thisAsset['assetTypes_productLink']);
-                if (thisAsset['assetTypes_productLink'] !== null) {
-                    $("#assetTypePageProductLink").attr("onclick", "cordova.InAppBrowser.open('" + thisAsset['assetTypes_productLink'] + "','_blank')");
-                }
-                $(thisAsset['tags']).each(function (index, element) {
-                    $("#assetTypePageAssetsList").append('<ons-list-item tappable modifier="longdivider"  onclick="document.querySelector(\'#myNavigator\').pushPage(\'asset.html\', {data: {id: ' + data.data.id + ',asset: ' + element['assets_id'] + '}});">' +
-                        '<div class="left">' +
-                        (element['flagsblocks']["COUNT"]["BLOCK"] > 0 ? '<ons-icon icon="fa-ban" style="color: #dc3545;"></ons-icon>&nbsp;' : '&nbsp;') +
-                        (element['flagsblocks']["COUNT"]["FLAG"] > 0 ? '<ons-icon icon="fa-flag" style="color: #ffc107;"></ons-icon>' : '') +
-                        '</div>' +
-                        '<div class="center">' + element['assets_tag_format'] + '</div>' +
-                        '</ons-list-item>');
-                });
-                //Thumbnails
-                var carousel = "";
-                $(thisAsset['thumbnails']).each(function (index, element) {
-                    myApp.functions.log(element);
-                    carousel += ('<ons-carousel-item><div style="margin-top: 20px;">' +
-                        '<img src="' + element.url + '" style="min-width:25%; max-width:100%;height: auto; max-height:65vh;" />' +
-                        '</div></ons-carousel-item>');
-                });
-                $("#assetTypePageCarouselTarget").html(carousel);
-                if (carousel != "") {
-                    $("#assetTypePageCarousel").parent().show();
-                    document.getElementById('assetTypePageCarousel').refresh();
-                } else {
-                    $("#assetTypePageCarousel").parent().hide();
-                }
-
-                //Files
-                $("#assetTypePageFilesList").html("");
-                if (myApp.auth.instanceHasPermission(54)) {
-                    $(thisAsset['files']).each(function (index, element) {
-                        myApp.functions.log(element);
-                        $("#assetTypePageFilesList").append('<ons-list-item tappable modifier="longdivider" onclick="myApp.functions.s3url(' + element['s3files_id'] + ',false,myApp.functions.openBrowser);">' +
+            var requestData = {"assetTypes_id": data.data.id,"all":true };
+            myApp.functions.apiCall("assets/list.php", requestData, function (assetDownloadResult) {
+                if (assetDownloadResult['assets'].length == 1) {
+                    myApp.controllers.assets.assetTypePagePullRefresh = document.getElementById('assetTypePagePullHook');
+                    myApp.controllers.assets.assetTypePagePullRefresh.addEventListener('changestate', function(event) {
+                        var message = '';
+                        switch (event.state) {
+                            case 'initial':
+                                message = 'Pull to refresh';
+                                break;
+                            case 'preaction':
+                                message = 'Release';
+                                break;
+                            case 'action':
+                                message = 'Loading...';
+                                break;
+                        }
+                        myApp.controllers.assets.assetTypePagePullRefresh.innerHTML = message;
+                    });
+                    myApp.controllers.assets.assetTypePagePullRefresh.onAction = function(done) {
+                        myApp.controllers.pages.assetTypePage({"data":data.data});
+                        done();
+                    };
+                    var thisAsset = assetDownloadResult['assets'][0];
+                    myApp.data.assetTypes[thisAsset['assetTypes_id']] = thisAsset;
+                    $("#assetTypePageTitle").html(thisAsset['assetTypes_name']);
+                    $("#assetTypePageManufacturer").html(thisAsset['manufacturers_name']);
+                    $("#assetTypePageCategory").html(thisAsset['assetCategories_name']);
+                    $("#assetTypePageDescription").html(myApp.functions.nl2br(thisAsset['assetTypes_description']));
+                    $("#assetTypePageProductLink").html(thisAsset['assetTypes_productLink']);
+                    if (thisAsset['assetTypes_productLink'] !== null) {
+                        $("#assetTypePageProductLink").attr("onclick", "cordova.InAppBrowser.open('" + thisAsset['assetTypes_productLink'] + "','_blank')");
+                    }
+                    $("#assetTypePageAssetsList").html("");
+                    $(thisAsset['tags']).each(function (index, element) {
+                        $("#assetTypePageAssetsList").append('<ons-list-item tappable modifier="longdivider"  onclick="document.querySelector(\'#myNavigator\').pushPage(\'asset.html\', {data: {id: ' + data.data.id + ',asset: ' + element['assets_id'] + '}});">' +
                             '<div class="left">' +
-                            '<ons-icon icon="' + myApp.functions.fileExtensionToIcon(element['s3files_extension']) + '"></ons-icon>' +
+                            (element['flagsblocks']["COUNT"]["BLOCK"] > 0 ? '<ons-icon icon="fa-ban" style="color: #dc3545;"></ons-icon>&nbsp;' : '&nbsp;') +
+                            (element['flagsblocks']["COUNT"]["FLAG"] > 0 ? '<ons-icon icon="fa-flag" style="color: #ffc107;"></ons-icon>' : '') +
                             '</div>' +
-                            '<div class="center">' + element['s3files_name'] + '</div>' +
-                            '<div class="right">' + myApp.functions.formatSize(element['s3files_meta_size']) + '</div>' +
+                            '<div class="center">' + element['assets_tag_format'] + '</div>' +
                             '</ons-list-item>');
                     });
-                }
-            } else {
+                    //Thumbnails
+                    var carousel = "";
+                    $(thisAsset['thumbnails']).each(function (index, element) {
+                        myApp.functions.log(element);
+                        carousel += ('<ons-carousel-item><div style="margin-top: 20px;">' +
+                            '<img src="' + element.url + '" style="min-width:25%; max-width:100%;height: auto; max-height:65vh;" />' +
+                            '</div></ons-carousel-item>');
+                    });
+                    $("#assetTypePageCarouselTarget").html(carousel);
+                    if (carousel != "") {
+                        $("#assetTypePageCarousel").parent().show();
+                        document.getElementById('assetTypePageCarousel').refresh();
+                    } else {
+                        $("#assetTypePageCarousel").parent().hide();
+                    }
 
-            }
+                    //Files
+                    $("#assetTypePageFilesList").html("");
+                    if (myApp.auth.instanceHasPermission(54)) {
+                        $(thisAsset['files']).each(function (index, element) {
+                            myApp.functions.log(element);
+                            $("#assetTypePageFilesList").append('<ons-list-item tappable modifier="longdivider" onclick="myApp.functions.s3url(' + element['s3files_id'] + ',false,myApp.functions.openBrowser);">' +
+                                '<div class="left">' +
+                                '<ons-icon icon="' + myApp.functions.fileExtensionToIcon(element['s3files_extension']) + '"></ons-icon>' +
+                                '</div>' +
+                                '<div class="center">' + element['s3files_name'] + '</div>' +
+                                '<div class="right">' + myApp.functions.formatSize(element['s3files_meta_size']) + '</div>' +
+                                '</ons-list-item>');
+                        });
+                    }
+                }
+            },false);
         },
         assetPage: function (data) {
             var thisAssetType = myApp.data.assetTypes[data.data.id];
@@ -339,8 +360,8 @@ myApp.controllers = {
             $("#assetPageWeekRate").html((thisAsset['assets_weekRate'] !== null ? thisAsset['assets_weekRate_format'] : thisAssetType['assetTypes_weekRate_format']));
             $("#assetPageDayRate").html((thisAsset['assets_dayRate'] !== null ? thisAsset['assets_dayRate_format'] : thisAssetType['assetTypes_dayRate_format']));
             $("#assetPageDefinableFields").html("");
-            for (i = 1; i <= 10; i++) {
-                if (thisAssetType['fields'][i-1] !== "") {
+            for (i = 1; i <= thisAssetType['fields'].length; i++) {
+                if (thisAssetType['fields'][i-1] !== "" && thisAsset["asset_definableFields_" + i] !== "") {
                     $("#assetPageDefinableFields").append('<ons-list-header>' + thisAssetType['fields'][i-1] + '</ons-list-header>' +
                         '        <ons-list-item modifier="nodivider">' +
                         '          <div class="center">' +
